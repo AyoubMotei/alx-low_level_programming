d078965b36c530ef90b97e101c56ea5c6b35089b
AyoubMotei <ayoub.motei@gmail.com>
2023-05-04 19:52:06 -0700

    0x14. C - Bit manipulation

--- 0x14-bit_manipulation/3-set_bit.c
+++ 0x14-bit_manipulation/3-set_bit.c
@@ -9,14 +9,10 @@
  */
 int set_bit(unsigned long int *n, unsigned int index)
 {
-	/** check if the index is within the bounds of the number */
 	if (index > 63)
 		return (-1);
 
-	/** set the bit at the given index to 1 by performing a bitwise OR with a 1 at the index */
 	*n = ((1UL << index) | *n);
-
-	/** return 1 to indicate success */
 	return (1);
 }
 
