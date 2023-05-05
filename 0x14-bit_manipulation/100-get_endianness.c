91a0c183e8d21d0ae73b20a64c1c64132b04864c
AyoubMotei <ayoub.motei@gmail.com>
2023-05-04 20:01:31 -0700

    0x14. C - Bit manipulation

--- 0x14-bit_manipulation/100-get_endianness.c
+++ 0x14-bit_manipulation/100-get_endianness.c
@@ -2,19 +2,13 @@
 
 /**
  * get_endianness - checks if a machine is little or big endian
- *
  * Return: 0 for big, 1 for little
  */
 int get_endianness(void)
 {
-    /* Create an unsigned integer with value 1 */
-    unsigned int i = 1;
+	unsigned int i = 1;
+	char *c = (char *) &i;
 
-    /* Get a pointer to the first byte of i */
-    char *c = (char *) &i;
-
-    /* If the value of the first byte of i is 1, the machine is little-endian */
-    /* Otherwise, the machine is big-endian */
-    return (*c);
+	return (*c);
 }
 
--- 0x14-bit_manipulation/4-clear_bit.c
+++ 0x14-bit_manipulation/4-clear_bit.c
@@ -7,19 +7,12 @@
  *
  * Return: 1 for success, -1 for failure
  */
-
 int clear_bit(unsigned long int *n, unsigned int index)
 {
-	/** Check if index is greater than 63, which is the maximum number of bits */
 	if (index > 63)
 		return (-1);
 
-	/** Clear the bit at the given index by using a bitwise NOT operator
-	and a left shift operation to create a mask with a 0 at the bit index
-	and all other bits set to 1. This mask is then ANDed with the original
-	 number to clear the bit at the given index.*/
 	*n = (~(1UL << index) & *n);
-
 	return (1);
 }
 
